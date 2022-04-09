3fec4d375f5140b569cde8031de6ab11e0c6241b
HaAnhNguyen99 <76512938+HaAnhNguyen99@users.noreply.github.com>
2022-04-09 11:14:30 +0700

    pull

--- Server/src/app/Model/token.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const token = new Schema({
-	token: { type: String, required: true },
-	date: { type: Date, default: Date.now() }
-})
-
-module.exports = mongoose.model('token', token)
