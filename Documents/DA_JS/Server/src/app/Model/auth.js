b166a6853714ad3fba7dbd9670c0cc72beabf7ac
HaAnhNguyen99 <76512938+HaAnhNguyen99@users.noreply.github.com>
2022-04-14 03:31:19 +0700

    commit master

--- Documents/DA_JS/Server/src/app/Model/auth.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const accountSchema = new Schema({
-	account: { type: String, required: true, unique: true },
-	password: { type: String, required: true }
-})
-const account = mongoose.model('accountdata', accountSchema)
-module.exports = account
--- Documents/DA_JS/Server/src/resources/views/partials/footer.hbs
+++
@@ -1,1 +0,0 @@
-<h1>day la footer</h1>
